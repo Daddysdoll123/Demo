cde5d499dc117e38373ab0b93a5108a9ffbf4c74
Dhanalaxmi Kotur <koturdhanalaxmi23@gmail.com>
2023-11-25 14:01:11 +0530

    Added 2 files

---
+++ RandomChai/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Random from './components/Random'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Random/>
+    </>
+  )
+}
+
+export default App
